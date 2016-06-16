30feab92fa7d72cf773bd221b619e01e37ab3b4e
Bradley Reeder <bradjohnreeder@gmail.com>
2016-06-16 04:47:48 +0100

    Update test.js
    tidied up the code, indentations, etc.

--- test.js
+++ test.js
@@ -1,46 +1,42 @@
-QUnit.test('testing the testing environment', function(assert){
-  var actual = 1;
-  var expected = 1;
-  assert.equal(actual, expected, 'test environment is set up correctly');
-});
-
-QUnit.test('testing if the initial time environment is correct', function(assert){
-  assert.equal(stopWatch.innerHTML, '02:00', 'correct initial time');
-  assert.equal(time.getMinutes(), 2, 'correct initial date in minutes');
-  assert.equal(time.getSeconds(), 0, 'correct initial date seconds');
-});
-
-QUnit.test('minutes decrement by 1 when seconds are 0',
-  function(assert) {
-    var done = assert.async();
-    var startTime = Number(stopWatch.innerHTML.split(':')[0]);
-    timer();
-    setTimeout(function(){
-      var timeNow = Number(stopWatch.innerHTML.split(':')[0]);
-      assert.equal(timeNow === startTime - 1, true, 'minutes decrement from 2 to 1');
-      done();
-    }, 1000);
+  QUnit.test('testing the testing environment', function(assert){
+    var actual = 1;
+    var expected = 1;
+    assert.equal(actual, expected, 'test environment is set up correctly');
   });
 
-QUnit.test('seconds decrement to 59 when seconds are 0',
-  function(assert){
-    var done = assert.async();
-    stopWatch.innerHTML = "2:0";
-    time = new Date('June 15, 2016 00:02:00');
-    var startTime = Number(stopWatch.innerHTML.split(':')[1]);
-    timer();
-    setTimeout(function(){
-      var timeNow = Number(stopWatch.innerHTML.split(':')[1]);
-      assert.equal(timeNow === startTime + 59, true, 'seconds decrements from 0 to 59');
-      done();
-    }, 1000);
+  QUnit.test('testing if the initial time environment is correct', function(assert){
+    assert.equal(stopWatch.innerHTML, '02:00', 'correct initial time');
+    assert.equal(time.getMinutes(), 2, 'correct initial date in minutes');
+    assert.equal(time.getSeconds(), 0, 'correct initial date seconds');
   });
- // can make this test more clever by saying it tests that with every
- // 1000ms the function decrements it by 1.
- // this is working as it is as the function is no longer recursive. you need to refactor it
- // with recursive behaviour.
- //i.e. make a test for recursive functionality.
-  QUnit.test('seconds continue to decrement with each function iteration',
+
+  QUnit.test('minutes decrement by 1 when seconds are 0',
+    function(assert) {
+      var done = assert.async();
+      var startTime = Number(stopWatch.innerHTML.split(':')[0]);
+      timer();
+      setTimeout(function(){
+        var timeNow = Number(stopWatch.innerHTML.split(':')[0]);
+        assert.equal(timeNow === startTime - 1, true, 'minutes decrement from 2 to 1');
+        done();
+      }, 1000);
+    });
+
+  QUnit.test('seconds decrement to 59 when seconds are 0',
+    function(assert){
+      var done = assert.async();
+      stopWatch.innerHTML = "2:0";
+      time = new Date('June 15, 2016 00:02:00');
+      var startTime = Number(stopWatch.innerHTML.split(':')[1]);
+      timer();
+      setTimeout(function(){
+        var timeNow = Number(stopWatch.innerHTML.split(':')[1]);
+        assert.equal(timeNow === startTime + 59, true, 'seconds decrements from 0 to 59');
+        done();
+      }, 1000);
+    });
+ 
+  QUnit.test('seconds continue to decrement with each set interval',
     function(assert){
       assert.expect(3);
       var done1 = assert.async();
@@ -80,9 +76,8 @@ QUnit.test('seconds decrement to 59 when seconds are 0',
       }, 1000);
     });
 
-
-QUnit.test('reset the time variables to default', function(assert){
-  expect(0);
-  stopWatch.innerHTML = "02:00";
-  time = new Date('June 15, 2016 00:02:00');
-});
+  QUnit.test('reset the time variables to default', function(assert){
+    expect(0);
+    stopWatch.innerHTML = "02:00";
+    time = new Date('June 15, 2016 00:02:00');
+  });
